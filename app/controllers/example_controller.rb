b13e6665cb3030bb491c9254e12786e19c64621d
pejot <pejot@pejot-linux-station.(none)>
2012-07-21 17:23:21 +0200

    preparing helpers and example view structure

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,31 +1,41 @@
+
 class ExampleController < ApplicationController
-  def home
+  def home    
   end
 
   def button1
+    render :layout => false
   end
 
   def button2
+    render :layout => false
   end
 
   def tab1
+    render :layout => false
   end
 
   def tab2
+    render :layout => false
   end
 
   def tab3
+    render :layout => false
   end
 
   def tab4
+    render :layout => false
   end
 
   def tab5
+    render :layout => false
   end
 
   def tab6
+    render :layout => false
   end
 
   def tab7
+    render :layout => false
   end
 end
--- app/helpers/ajax_view_helper.rb
+++ app/helpers/ajax_view_helper.rb
@@ -1,2 +1,42 @@
 module AjaxViewHelper
+  
+  @@ui_element_css_class = "ui_element"
+  @@ui_history_element_css_class = "ui_history_element"
+  @@ui_container_element_css_class = "ui_container"
+  @@ui_body_element_css_class ="ui_body_id"
+  
+  def ajax_link(*args, &block)
+    inject_css(args, @@ui_element_css_class + " " + @@ui_history_element_css_class)
+    return link_to(*args, &block)
+   
+  end
+  
+  def ajax_button(*args, &block)
+    inject_css(args, @@ui_element_css_class)
+    result = link_to(*args, &block)
+    return ("<button" + result[2..-3] + "button>").html_safe
+  end
+  
+  def ajax_container(*args, &block)
+    inject_css(args, @@ui_container_element_css_class)
+    return (tag("div",*args, &block)[0..-1] + capture(&block) + "</div>").html_safe 
+  end
+  
+  def ajax_body(&block)
+    return (tag("div", {:id => @@ui_body_element_css_class })[0..-1] + capture(&block) + "</div>").html_safe 
+  end
+  
+  private 
+  def inject_css(args,css_class)
+  for arg in args
+      if arg.is_a? Hash
+        if arg.has_key?(:class)
+          arg[:class] += " " + css_class
+        else
+          arg[:class] = css_class
+        end
+      end
+    end
+  end
+  
 end
